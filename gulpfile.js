49c91d7c590ce230f5632bd4363b6d7fbc4baad2
Nicholas MacArthur <n.j.macarthur@gmail.com>
2019-08-23 11:31:31 +1000

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const { src, dest, watch, parallel } = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const minifyCSS = require('gulp-csso');
+
+function full() {
+  return src('./src/stylekit-full.scss')
+    .pipe(sass())
+    .pipe(minifyCSS())
+    .pipe(dest('./dist'));
+}
+
+function nolayout() {
+  return src('./src/stylekit-nolayout.scss')
+    .pipe(sass())
+    .pipe(minifyCSS())
+    .pipe(dest('./dist'));
+}
+
+function type() {
+  return src('./src/stylekit-type.scss')
+    .pipe(sass())
+    .pipe(minifyCSS())
+    .pipe(dest('./dist'));
+}
+
+exports.full = full;
+exports.nolayout = nolayout;
+exports.type = type;
+exports.default = parallel(full, nolayout, type);
+exports.watch = () => {
+  watch('./src/**/*.scss', parallel(full, nolayout, type));
+};
